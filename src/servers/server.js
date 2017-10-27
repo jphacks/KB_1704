674eb2c7cd9b33e3f6572ad4a40dd30996e88c5e
栗原佳輝 <yosyos@kuriharakeiakiranoMacBook-Pro.local>
2017-10-27 09:16:40 +0900

    upload server program

--- src/servers/server.js
+++ src/servers/server.js
@@ -5,41 +5,27 @@ var server = http.createServer();
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var IDandEmotion = new Map();
 var EmotionQueue;
-var emotions = ["anger", "contempt", "disgust", "fear", "happiness", "neutral", "sadness", "surprise"];
+var emotions = ["anger", "happiness", "neutral","surprise"];
 
 function getFaceId(srcImg,res) {
     var request = new XMLHttpRequest();
     
     request.onreadystatechange = function () {
-		switch (request.readyState) {	    
-		case 0:
-			console.log('uninitialized!');
-			break;
-		case 1:
-			console.log('loading...');
-			break;
-		case 2: 
-			console.log('loaded.');
-			break;
-		case 3: 
-			console.log('interactive... ' + request.responseText.length + ' bytes.');
-			break;
-		case 4: 
-			if (request.status == 200 || request.status == 304) {
-				console.log('COMPLETE!');
-				var data = JSON.parse(request.responseText);
-				sentIdentify(data[0].faceId,res);
-			} else {
-				console.log('Failed. HttpStatus: ' + request.responseText);
-			}
-			break;
-		}
+	if(request.readyState ==4){
+	    if (request.status == 200 || request.status == 304) {
+		var data = JSON.parse(request.responseText);
+		sentIdentify(data[0].faceId,res);
+	    } else {
+		console.log('Failed. HttpStatus: ' + request.responseText);
+		getFaceId(srcImg,res);
+	    }
+	}
     };
     request.open("POST", "https://eastasia.api.cognitive.microsoft.com/face/v1.0/detect?true&false&gender");
     request.setRequestHeader("Content-Type", "application/json");
     request.setRequestHeader("Ocp-Apim-Subscription-Key", "bcfb3a73caf944a095b8a6c41be5f7b7");
     request.addEventListener("error", () => {
-		console.error("Network Error");
+	console.error("Network Error");
     });
     s = "https://s3-ap-northeast-1.amazonaws.com/facard-photo/" + srcImg;
     console.log("URL of Img File is " + s);
@@ -49,46 +35,33 @@ function getFaceId(srcImg,res) {
 function getEmotion(srcImg,res) {
     var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
-        switch (request.readyState) {
-        case 0:
-            console.log('uninitialized!');
-            break;
-        case 1:
-            console.log('loading...');
-            break;
-        case 2:
-            console.log('loaded.');
-            break;
-        case 3:
-            console.log('interactive... ' + request.responseText.length + ' bytes.');
-            break;
-        case 4:
-            if (request.status == 200 || request.status == 304) {
-                console.log('COMPLETE!');
+	if(request.readyState ==4){
+	    if (request.status == 200 || request.status == 304) {
                 var data = JSON.parse(request.responseText);
                 var emo = data[0].faceAttributes.emotion[EmotionQueue];
                 console.log(data[0].faceAttributes.emotion);
-                var s = "false";
+		
+		var retext = '{"status":failed}';
                 if (Number(emo) >= 0.7) {
                     console.log("SUCCESS!!!");
-                    s = "true";
+		    retext = '{"status":success}';
                 }
-
-                res.writeHead(200, {
-                    'Content-Type': 'text/plain',
-                    'charset': 'utf-8'
-                });
-                res.write(JSON.parse(s));
-                res.end();
+		res.writeHead(200, {
+		    'Content-Type': 'application/json',
+		    'charset': 'utf-8'
+		});
+		
+		res.end(retext);
             } else {
                 console.log('Failed. HttpStatus: ' + request.responseText);
+		getEmotion(srcImg,res);
             }
-            break;
-        }
+	}
     };
     request.open("POST", "https://eastasia.api.cognitive.microsoft.com/face/v1.0/detect?true&false&returnFaceAttributes=emotion");
     request.setRequestHeader("Content-Type", "application/json");
     request.setRequestHeader("Ocp-Apim-Subscription-Key", "bcfb3a73caf944a095b8a6c41be5f7b7");
+
     request.addEventListener("error", () => {
         console.error("Network Error");
     });
@@ -98,43 +71,34 @@ function getEmotion(srcImg,res) {
 }
 
 function getUserfromDB(person_id,res){
-	var request = new XMLHttpRequest();
+    var request = new XMLHttpRequest();
 
-	request.onreadystatechange = function () {
-		switch (request.readyState) {
-		case 0:
-
-			console.log('uninitialized!');
-			break;
-		case 1: 
-			console.log('loading...');
-			break;
-		case 2: 
-			console.log('loaded.');
-			break;
-		case 3: 
-			console.log('interactive... ' + request.responseText.length + ' bytes.');
-			break;
-		case 4: 
-			if (request.status == 200 || request.status == 304) {
-				console.log('COMPLETE!');
-				console.log(request.responseText);
-				res.writeHead(200, {
-					'Content-Type': 'application/json',
-					'charset': 'utf-8'
-				});
-				res.write(request.responseText);
-				res.end();
-			} else {
-				console.log('Failed. HttpStatus: ' + request.statusText);
-			}
-			break;
-		}
+    request.onreadystatechange = function () {
+	if(request.readyState ==4){
+	    if (request.status == 200 || request.status == 304) {
+		console.log('COMPLETE!');
+		var emo = emotions[Math.floor(Math.random() * emotions.length)];
+		EmotionQueue = emo;
+		res.writeHead(200, {
+		    'Content-Type': 'application/json',
+		    'charset': 'utf-8'
+		});
+		var add_data = {'emotion': emo}
+		var test_json = JSON.parse(request.responseText)
+		var a =test_json.slice( 0, -2 ) ;
+		a = a + ',"emotion":"'+emo+'"}]'
+		console.log(JSON.stringify(a));
+		res.write(JSON.stringify(a));
+		res.end();
+	    } else {
+		console.log('Failed. HttpStatus: ' + request.statusText);
+	    }
+	}
     };
 
-	request.open("GET", "https://8kw9j63717.execute-api.ap-northeast-1.amazonaws.com/test/users?faceid="+person_id);
-	request.setRequestHeader("Content-Type", "application/json");
-	request.send()
+    request.open("GET", "https://8kw9j63717.execute-api.ap-northeast-1.amazonaws.com/test/users?faceid="+person_id);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.send()
 }
 
 function sentIdentify(data,res) {
@@ -142,43 +106,28 @@ function sentIdentify(data,res) {
     var request = new XMLHttpRequest();
 
     request.onreadystatechange = function () {
-		switch (request.readyState) {
-		case 0:
-
-			console.log('uninitialized!');
-			break;
-		case 1: 
-			console.log('loading...');
-			break;
-		case 2: 
-			console.log('loaded.');
-			break;
-		case 3: 
-			console.log('interactive... ' + request.responseText.length + ' bytes.');
-			break;
-		case 4: 
-			if (request.status == 200 || request.status == 304) {
-				console.log('COMPLETE!');
-				console.log(request.responseText);
-				var result = JSON.parse(request.responseText);
-				var FACE_ID = result[0].faceId;
-				var PERSON_ID = result[0].candidates[0].personId;
-				var CONFIDENCE = result[0].candidates[0].confidence;
-				console.log("FACE_ID=" + FACE_ID);
-				console.log("PERSON_ID=" + PERSON_ID);
-				console.log("CONFIDENCE=" + CONFIDENCE);
-				getUserfromDB(PERSON_ID,res);
-			} else {
-				console.log('Failed. HttpStatus: ' + request.statusText);
-			}
-			break;
-		}
+	if(request.readyState ==4){
+	    if (request.status == 200 || request.status == 304) {
+		console.log('COMPLETE!');
+		console.log(request.responseText);
+		var result = JSON.parse(request.responseText);
+		var FACE_ID = result[0].faceId;
+		var PERSON_ID = result[0].candidates[0].personId;
+		var CONFIDENCE = result[0].candidates[0].confidence;
+		console.log("FACE_ID=" + FACE_ID);
+		console.log("PERSON_ID=" + PERSON_ID);
+		console.log("CONFIDENCE=" + CONFIDENCE);
+		getUserfromDB(PERSON_ID,res);
+	    } else {
+		console.log('Failed. HttpStatus: ' + request.statusText);
+	    }
+	}
     };
     request.open("POST", " https://eastasia.api.cognitive.microsoft.com/face/v1.0/identify?");
     request.setRequestHeader("Content-Type", "application/json");
     request.setRequestHeader("Ocp-Apim-Subscription-Key", "bcfb3a73caf944a095b8a6c41be5f7b7");
     request.addEventListener("error", () => {
-		console.error("Network Error");
+	console.error("Network Error");
     });
     var array = [data];
     var s = "{'faceIds': " + "['"+array +"']"+ ",'personGroupId':'facardpersongroup'}";
@@ -191,15 +140,15 @@ server.on('request', function (req, res) {
     var query = url.parse(req.url).query;
     console.log("access log:" + uri)
     if(uri == "/personalId"){
-		switch (req.method) {
-		case 'GET':
-			console.log("personalId:"+query.substr(query.indexOf('=')+1,query.length));
-			getFaceId(query.substr(query.indexOf('=')+1,query.length),res);
-			break;
-		default:
-			console.log("DEFAULT");
-			break;
-		}
+	switch (req.method) {
+	case 'GET':
+	    console.log("personalId:"+query.substr(query.indexOf('=')+1,query.length));
+	    getFaceId(query.substr(query.indexOf('=')+1,query.length),res);
+	    break;
+	default:
+	    console.log("DEFAULT");
+	    break;
+	}
     } else  if (uri == "/emotion") {
         switch (req.method) {
         case 'GET':
@@ -210,7 +159,7 @@ server.on('request', function (req, res) {
             console.log("DEFAULT");
             break;
         }
-        res.write('NOTE');
+
     }
     // console.log(uri)
     
